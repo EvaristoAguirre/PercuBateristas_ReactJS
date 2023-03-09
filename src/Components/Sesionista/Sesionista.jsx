0319007890eae3ec8e06a3c27d65703d90265704
Evaristo.Aguirre <dasbrotchen@gmail.com>
2023-03-09 00:31:21 -0300

    finalizcion-medias-queries

--- src/Components/Sesionista/Sesionista.jsx
+++ src/Components/Sesionista/Sesionista.jsx
@@ -17,14 +17,47 @@ const Sesionista = () => {
                 <div className='sesiones-tarjeta-planes'>
                     <div className='sesiones-planes_izq'>
                         <img src="./muestra_A.jpg" alt=""/>
+                        <h4>Plan Medio</h4>
+                        <h5>- 1 tema -</h5>
+                        
+                        <div className='sesiones-planes-capa'>
+                            
+                            <p>1 reunión de 1hs por Zoom/Meet.
+                                <br />
+                                Formulario para afinar detalles.
+                                <br />
+                                2 correcciones.
+                                <br />Entrega: 2 semanas.
+                                <br />
+                                Percusión latinoamericana y/o bateria.
+                                <br />
+                                Contacto directo por WhatsApp.
+                            </p>
+                        </div>
                     </div>
                     
-                    <div className='sesiones-planes_cen' >
+                    {/* <div className='sesiones-planes_cen' >
                         <img src="./muestra_B.jpg" alt=""/>
-                    </div>
+                    </div> */}
                     
                     <div className='sesiones-planes_der'>
                         <img src="./hero-muestra.jpg" alt="" />
+                        <h4>Plan Disco</h4>
+                        <h5>- hasta 5 temas -</h5>
+                        <div className='sesiones-planes-capa'>
+                            
+                            <p>2 reuniones de 1hs por Zoom/Meet.
+                                <br />
+                                Formulario para afinar detalles.
+                                <br />
+                                5 correcciones.
+                                <br />Entrega: 4 semanas.
+                                <br />
+                                Percusión latinoamericana y/o bateria.
+                                <br />
+                                Contacto directo por WhatsApp.
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className='sesiones-tarjeta-texto'>
